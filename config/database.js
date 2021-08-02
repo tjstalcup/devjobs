94f6ea34173e8e126fead58574c33e4c85e02ae3
TJ Stalcup <tstalcupjr@gmail.com>
2021-08-02 16:09:51 -0400

    removes secrets

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 module.exports = {
-
   // 'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@url:27017/dbname
-  'url' : "mongodb://devjobs:devjobs1234@cluster0-shard-00-00-ucr4u.mongodb.net:27017,cluster0-shard-00-01-ucr4u.mongodb.net:27017,cluster0-shard-00-02-ucr4u.mongodb.net:27017/devjobs?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
-  'testurl' : "mongodb://devjobs:devjobs1234@cluster0-shard-00-00-ucr4u.mongodb.net:27017,cluster0-shard-00-01-ucr4u.mongodb.net:27017,cluster0-shard-00-02-ucr4u.mongodb.net:27017/devjobstest?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
+  // env
 };
